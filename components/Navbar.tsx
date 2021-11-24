dec45f1988c21306c0f4a4ddd96f3a0f1f67c036
Fabio <alessf13@gmail.com>
2021-11-24 13:40:59 +0300

    feat: elements spread in divs within navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,28 @@ import logo from "../public/logo.svg";
 
 function navbar() {
   return (
-    <nav>
-      <Image
-        src={logo}
-        alt={`Logo de le Kebab Rose`}
-        width={200}
-        height={200}
-      />
-      <div></div>
+    <nav className="navbar">
+      <div className="logo">
+        <h1>+3309324242</h1>
+        <Image
+          src={logo}
+          alt={`Logo de le Kebab Rose`}
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="navContent">
+        <div className="list">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+          </ul>
+        </div>
+        <div className="cart">
+          <h3>Cart</h3>
+        </div>
+      </div>
     </nav>
   );
 }
