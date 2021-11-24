7a450cc62c789e3d410ad31a8ab645c8ffd85693
Fabio <alessf13@gmail.com>
2021-11-24 12:50:50 +0300

    feat: logo added to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
+import logo from "../public/logo.svg";
+
 function navbar() {
   return (
     <nav>
-      <div className="logo">
-        <img src="./logo.svg" alt="Logo de Le Kebab Rose" />
-      </div>
+      <Image
+        src={logo}
+        alt={`Logo de le Kebab Rose`}
+        width={200}
+        height={200}
+      />
       <div></div>
     </nav>
   );
