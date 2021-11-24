58679c7427432eec38a306ec04c469ddc7b3b16a
Fabio <alessf13@gmail.com>
2021-11-25 00:36:25 +0300

    feat: added menu element titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h2>sth</h2>;
+  return (
+    <section>
+      <div className="menu">
+        <div>
+          <h3>ENTREE</h3>
+        </div>
+        <div>
+          <h3>SANDWICH</h3>
+        </div>
+        <div>
+          <h3>PANINI</h3>
+        </div>
+        <div>
+          <h3>BURGER</h3>
+        </div>
+        <div>
+          <h3>TEX MEX</h3>
+        </div>
+        <div>
+          <h3>ASSIETTE</h3>
+        </div>
+        <div>
+          <h3>DESSERT</h3>
+        </div>
+        <div>
+          <h3>BOISSON</h3>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
