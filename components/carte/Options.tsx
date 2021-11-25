6c198604bcacc13b33d3cd469f23b09f3dbc0ac4
Fabio <alessf13@gmail.com>
2021-11-25 16:46:36 +0300

    feat: options added for salads

--- components/carte/Options.tsx
+++ components/carte/Options.tsx
@@ -1,5 +1,32 @@
 function Options() {
-  return <h3>options</h3>;
+  return (
+    <section className="options">
+      <div className="option">
+        <h3>Salada tunisienne</h3>
+        <h3>5.50€</h3>
+      </div>
+      <div className="option">
+        <h3>Salada orientale</h3>
+        <h3>5.50€</h3>
+      </div>
+      <div className="option">
+        <h3>Salada au poulet</h3>
+        <h3>5.50€</h3>
+      </div>
+      <div className="option">
+        <h3>Salada au thon</h3>
+        <h3>5.50€</h3>
+      </div>
+      <div className="option">
+        <h3>Brik a l'oeuf</h3>
+        <h3>5.50€</h3>
+      </div>
+      <div className="option">
+        <h3>Bourrek</h3>
+        <h3>5.50€</h3>
+      </div>
+    </section>
+  );
 }
 
 export default Options;
