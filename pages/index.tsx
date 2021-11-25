e2ac85f4e1561fac6f7d48f3d4d4ac57a3d80f07
Fabio <alessf13@gmail.com>
2021-11-25 12:30:29 +0300

    styles: images centered in carte section

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,41 +13,41 @@ const Home: NextPage = () => {
     <section>
       <div className="menu">
         <div>
-          <Image src={salad} alt={``} width={200} height={200} />
+          <Image src={salad} alt={``} width={100} height={100} />
 
           <h3>ENTREE</h3>
         </div>
         <div>
-          <Image src={sandwich} alt={``} width={200} height={200} />
+          <Image src={sandwich} alt={``} width={100} height={100} />
 
           <h3>SANDWICH</h3>
         </div>
         <div>
-          <Image src={sandwich} alt={``} width={200} height={200} />
+          <Image src={sandwich} alt={``} width={100} height={100} />
 
           <h3>PANINI</h3>
         </div>
         <div>
-          <Image src={burger} alt={`Pink burger`} width={200} height={200} />
+          <Image src={burger} alt={`Pink burger`} width={100} height={100} />
           <h3>BURGER</h3>
         </div>
         <div>
-          <Image src={taco} alt={``} width={200} height={200} />
+          <Image src={taco} alt={``} width={100} height={100} />
 
           <h3>TEX MEX</h3>
         </div>
         <div>
-          <Image src={kebab} alt={``} width={200} height={200} />
+          <Image src={kebab} alt={``} width={100} height={100} />
 
           <h3>ASSIETTE</h3>
         </div>
         <div>
-          <Image src={dessert} alt={``} width={200} height={200} />
+          <Image src={dessert} alt={``} width={100} height={100} />
 
           <h3>DESSERT</h3>
         </div>
         <div>
-          <Image src={drink} alt={``} width={200} height={200} />
+          <Image src={drink} alt={``} width={100} height={100} />
 
           <h3>BOISSON</h3>
         </div>
