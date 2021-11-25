72eed1b01c16788b8395050a73a43d571cf6dd32
Fabio <alessf13@gmail.com>
2021-11-25 17:16:47 +0300

    refactor: Entree extracted into its own component

--- components/carte/Options.tsx
+++ components/carte/Options.tsx
@@ -1,32 +1,7 @@
+import Entree from "./options/entree";
+
 function Options() {
-  return (
-    <section className="options">
-      <div className="option">
-        <h3>Salada tunisienne</h3>
-        <h3>5.50€</h3>
-      </div>
-      <div className="option">
-        <h3>Salada orientale</h3>
-        <h3>5.50€</h3>
-      </div>
-      <div className="option">
-        <h3>Salada au poulet</h3>
-        <h3>5.50€</h3>
-      </div>
-      <div className="option">
-        <h3>Salada au thon</h3>
-        <h3>5.50€</h3>
-      </div>
-      <div className="option">
-        <h3>Brik a l'oeuf</h3>
-        <h3>5.50€</h3>
-      </div>
-      <div className="option">
-        <h3>Bourrek</h3>
-        <h3>5.50€</h3>
-      </div>
-    </section>
-  );
+  return <Entree />;
 }
 
 export default Options;
